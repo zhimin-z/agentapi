012c4396c341379012bc4871f5b16cb305691d01
Hugo Dutka <hugo@coder.com>
2025-04-04 13:47:40 +0200

    POC of command execution in an in-memory terminal emulator

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -12,7 +11,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/creack/pty"
+	"github.com/ActiveState/termtest/xpty"
 	"github.com/spf13/cobra"
 	"golang.org/x/term"
 )
@@ -36,52 +35,58 @@ var runCmd = &cobra.Command{
 
 		fmt.Printf("Running: %s %s\n", program, strings.Join(programArgs, " "))
 
-		// Create command
+		xp, _ := xpty.New(80, 1000, true)
+		defer xp.Close()
+
 		execCmd := exec.Command(program, programArgs...)
-
-		// Create pty
-		ptmx, err := pty.Start(execCmd)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error starting pty: %v\n", err)
+		if err := xp.StartProcessInTerminal(execCmd); err != nil {
+			fmt.Fprintf(os.Stderr, "Error starting process in terminal: %v\n", err)
 			os.Exit(1)
 		}
-		defer ptmx.Close()
+		execCmd.Stdin = os.Stdin
 
-		// Buffer to store the output
-		var outputBuffer bytes.Buffer
-
-		// Determine output file path
-		if outputFile == "" {
-			// Generate default output file name based on timestamp
-			timestamp := time.Now().Format("20060102-150405")
-			outputFile = filepath.Join(".", fmt.Sprintf("output-%s.log", timestamp))
-		}
-
-		// Create or open the output file
-		f, err := os.Create(outputFile)
+		snapshotFilePath := filepath.Join(".", fmt.Sprintf("snapshot-%s.log", time.Now().Format("20060102-150405")))
+		snapshotFile2Path := filepath.Join(".", fmt.Sprintf("snapshot-%s-2.log", time.Now().Format("20060102-150405")))
+		snapshotFile, err := os.Create(snapshotFilePath)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error creating output file: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Error creating snapshot file: %v\n", err)
 			os.Exit(1)
 		}
-		defer f.Close()
+		defer snapshotFile.Close()
+		snapshotFile2, err := os.Create(snapshotFile2Path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating snapshot file: %v\n", err)
+			os.Exit(1)
+		}
+		defer snapshotFile2.Close()
 
-		fmt.Printf("Saving output to: %s\n", outputFile)
-
-		// Use MultiWriter to write to both stdout, buffer and file
-		mw := io.MultiWriter(os.Stdout, &outputBuffer, f)
-
-		// Handle window size changes
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, syscall.SIGWINCH)
 		go func() {
-			for range ch {
-				if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
-					fmt.Fprintf(os.Stderr, "Error resizing pty: %v\n", err)
+			// this will block until the command is finished
+			if _, err := xp.WriteTo(snapshotFile2); err != nil && err != io.EOF {
+				fmt.Fprintf(os.Stderr, "Error writing snapshot to buf: %v\n", err)
+				os.Exit(1)
+			}
+		}()
+
+		go func() {
+			fmt.Println("Starting snapshot loop")
+			for {
+				time.Sleep(100 * time.Millisecond)
+				// Truncate file and seek to beginning to overwrite completely
+				if err := snapshotFile.Truncate(0); err != nil {
+					fmt.Fprintf(os.Stderr, "Error truncating snapshot file: %v\n", err)
+					os.Exit(1)
+				}
+				if _, err := snapshotFile.Seek(0, 0); err != nil {
+					fmt.Fprintf(os.Stderr, "Error seeking in snapshot file: %v\n", err)
+					os.Exit(1)
+				}
+				if _, err := snapshotFile.Write([]byte(xp.State.String())); err != nil {
+					fmt.Fprintf(os.Stderr, "Error writing snapshot to file: %v\n", err)
+					os.Exit(1)
 				}
 			}
 		}()
-		// Initial resize
-		ch <- syscall.SIGWINCH
 
 		// Set stdin in raw mode
 		oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
@@ -90,9 +95,6 @@ var runCmd = &cobra.Command{
 			os.Exit(1)
 		}
 		defer term.Restore(int(os.Stdin.Fd()), oldState)
-
-		// Channel to signal when the command is done
-		cmdDone := make(chan struct{})
 
 		// Handle SIGINT (Ctrl+C) and send it to the process
 		signalCh := make(chan os.Signal, 1)
@@ -106,38 +108,58 @@ var runCmd = &cobra.Command{
 			}
 		}()
 
-		// Handle output from the process
-		go func() {
-			io.Copy(mw, ptmx)
-			close(cmdDone)
-		}()
-
 		// Handle input to the process
 		go func() {
-			io.Copy(ptmx, os.Stdin)
-			// Don't close anything here - the process might still be running
+			io.Copy(xp.TerminalInPipe(), os.Stdin)
 		}()
 
-		// Wait for command to finish
-		cmdErr := execCmd.Wait()
-		
-		// Wait for all output to be processed
-		<-cmdDone
-		
-		// Stop handling signals
-		signal.Stop(ch)
-		signal.Stop(signalCh)
-		close(ch)
-		close(signalCh)
+		// Channel to receive the result of cmd.Wait()
+		cmdDone := make(chan error, 1)
+		go func() {
+			cmdDone <- execCmd.Wait()
+		}()
 
-		// Restore terminal before printing final message
-		term.Restore(int(os.Stdin.Fd()), oldState)
-		
-		if cmdErr != nil {
-			fmt.Fprintf(os.Stderr, "\nCommand exited with error: %v\n", cmdErr)
-			os.Exit(1)
+		// Channel to receive OS signals
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+		fmt.Println("Waiting for command to finish...")
+
+		select {
+		case err := <-cmdDone:
+			if err != nil {
+				// Check if the error is due to the process being killed by a signal
+				if exitErr, ok := err.(*exec.ExitError); ok {
+					if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+						// Don't print an error if killed by SIGTERM or SIGINT, as that's expected on signal
+						if status.Signaled() && (status.Signal() == syscall.SIGTERM || status.Signal() == syscall.SIGINT) {
+							fmt.Printf("Command terminated by signal: %s\n", status.Signal())
+						} else {
+							fmt.Fprintf(os.Stderr, "Command finished with error: %v\n", err)
+						}
+					} else {
+						fmt.Fprintf(os.Stderr, "Command finished with error: %v\n", err)
+					}
+				} else {
+					fmt.Fprintf(os.Stderr, "Command finished with error: %v\n", err)
+				}
+			} else {
+				fmt.Println("Command finished successfully.")
+			}
+		case sig := <-sigChan:
+			fmt.Printf("\nReceived signal: %v. Cleaning up and exiting...\n", sig)
+			if err := execCmd.Process.Signal(sig); err != nil {
+				fmt.Fprintf(os.Stderr, "Error signaling command: %v\n", err)
+				os.Exit(1)
+			}
+			if err := xp.Close(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error closing terminal: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Println("Terminal closed")
+			<-cmdDone // Wait for the command to actually exit after signaling
+			fmt.Println("Command finished")
 		}
 
-		fmt.Printf("\nCommand completed successfully. Output captured to %s\n", outputFile)
 	},
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,13 @@ require (
 )
 
 require (
+	github.com/ActiveState/termtest/conpty v0.5.0 // indirect
+	github.com/ActiveState/termtest/xpty v0.6.0 // indirect
+	github.com/ActiveState/vt10x v1.3.1 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/Netflix/go-expect v0.0.0-20200312175327-da48e75238e2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pty v1.1.8 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 )
