39efc931f2eaeb1c0b5cfc3fd53d6ffa76a68f97
BrunoQuaresma <bruno_nonato_quaresma@hotmail.com>
2025-06-03 14:37:27 +0000

    Fix build

--- chat/src/app/embed/page.tsx
+++ chat/src/app/embed/page.tsx
@@ -1,12 +1,19 @@
 import { Chat } from "@/components/chat";
 import { ChatProvider } from "@/components/chat-provider";
+import { Suspense } from "react";
 
 export default function EmbedPage() {
   return (
-    <ChatProvider>
-      <div className="flex flex-col h-svh">
-        <Chat />
-      </div>
-    </ChatProvider>
+    <Suspense
+      fallback={
+        <div className="text-center p-4 text-sm">Loading chat interface...</div>
+      }
+    >
+      <ChatProvider>
+        <div className="flex flex-col h-svh">
+          <Chat />
+        </div>
+      </ChatProvider>
+    </Suspense>
   );
 }
--- chat/src/app/page.tsx
+++ chat/src/app/page.tsx
@@ -1,14 +1,21 @@
 import { Chat } from "@/components/chat";
 import { ChatProvider } from "@/components/chat-provider";
 import { Header } from "./header";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <ChatProvider>
-      <div className="flex flex-col h-svh">
-        <Header />
-        <Chat />
-      </div>
-    </ChatProvider>
+    <Suspense
+      fallback={
+        <div className="text-center p-4 text-sm">Loading chat interface...</div>
+      }
+    >
+      <ChatProvider>
+        <div className="flex flex-col h-svh">
+          <Header />
+          <Chat />
+        </div>
+      </ChatProvider>
+    </Suspense>
   );
 }
