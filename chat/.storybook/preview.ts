8f7db1aa5fc33a3a8155ec46f0e9ea1c5788645e
Hugo Dutka <hugo@coder.com>
2025-07-22 14:39:35 +0200

    chore: add storybook (#33)

---
+++ chat/.storybook/preview.ts
@@ -0,0 +1,26 @@
+import { Preview } from "@storybook/nextjs";
+
+import { withThemeByClassName } from "@storybook/addon-themes";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "dark",
+    }),
+  ],
+};
+
+export default preview;
---
+++ chat/src/stories/message-input.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+
+import MessageInput from "../components/message-input";
+
+const meta = {
+  title: "Components/MessageInput",
+  component: MessageInput,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+} satisfies Meta<typeof MessageInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    onSendMessage: () => {},
+  },
+};
