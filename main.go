6aed263da1ff4b2f4eff466eae626bbe0e033a88
Cian Johnston <cian@coder.com>
2025-09-02 20:40:24 +0100

    chore: automaticalLy set version

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 //go:generate sh -c "go run main.go server --print-openapi dummy > openapi.json"
-
+//go:generate ./set_version.sh
 import "github.com/coder/agentapi/cmd"
 
 func main() {
---
+++ set_version.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if ! command -v go >/dev/null 2>&1; then
+    echo "go is required to run this script" && exit 1
+elif ! command -v jq >/dev/null 2>&1; then
+    echo "jq is required to run this script" && exit 1
+fi
+
+version=$(go run main.go --version | awk '{print $3}')
+
+jq --arg version "${version}" '.info.version = $version' openapi.json > openapi.json.tmp && mv openapi.json.tmp openapi.json
+jq --arg version "${version}" '.version = $version' chat/package.json > chat/package.json.tmp && mv chat/package.json.tmp chat/package.json
